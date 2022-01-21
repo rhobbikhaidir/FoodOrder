703a41ecd87444d9250e1d839f9e2de776298c92
rhobbikhaidir <rhobbie09@gmail.com>
2022-01-21 17:48:00 +0700

    create components meals

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import classes from "./Header.module.css";
+
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1 className={classes.title}>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
