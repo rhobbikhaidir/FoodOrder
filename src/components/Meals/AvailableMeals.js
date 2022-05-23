e244a2dcb1ee591b4f964f89faaba3ef324ba2bb
rhobbikhaidir <rhobbie09@gmail.com>
2022-05-23 17:45:09 +0700

    add animation loading

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,38 +3,13 @@ import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setLoading(true);
       const response = await fetch(
         "https://tesapp-d0cbe-default-rtdb.firebaseio.com/meals.json"
       );
@@ -53,10 +28,19 @@ const AvailableMeals = () => {
 
       console.log(loadedMeals);
       setMeals(loadedMeals);
+      setLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (loading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
